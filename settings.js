e16cf1c873a6bc746be88a4bc55adfd60b287b76
yeshanbbhdevil <yeshanbasnayake20071001@gmail.com>
2024-10-02 15:55:17 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "649107894179"
+global.ownername = "Dark Project"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '649107894179'  //creator number
+global.ownername = 'Dark Project' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
