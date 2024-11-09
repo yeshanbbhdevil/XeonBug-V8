2ba6da486aece756bcd4825274bda88babde2dc8
yeshanbbhdevil <yeshanbasnayake20071001@gmail.com>
2024-11-09 08:48:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "94725058757"
+global.ownername = "FUCKYOU"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '94725058757'  //creator number
+global.ownername = 'FUCKYOU' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
